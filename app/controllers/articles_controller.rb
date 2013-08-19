e63c2e897fdc678433b03723a732c473040bd0f1
jd pagano <jonathanpagano@gmail.com>
2013-08-18 22:22:54 -0400

    Update to articles CRUD. Finish current_user and require_current_user application controller methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,26 @@
 class ArticlesController < ApplicationController
+  before_filter :require_current_user
+
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 end
