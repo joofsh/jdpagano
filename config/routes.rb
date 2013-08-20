db6df7016ff394ef67eb0753c67cada6d8f05b11
jd pagano <jonathanpagano@gmail.com>
2013-08-20 13:49:45 -0400

    Reset root page to index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Jdpagano::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-   root 'main#about'
+   root 'main#index'
 
    get 'login' => 'sessions#new', as: 'login'
    get 'logout' => 'sessions#destroy', as: 'logout'
