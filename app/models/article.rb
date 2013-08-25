519cf576ffded38ae36e56e4d523f26892f24f74
jd pagano <jonathanpagano@gmail.com>
2013-08-25 16:31:38 -0400

    Create generate_slug method, slug field, and slug index on Article for SEO friendly urls

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,16 +2,27 @@ class Article
   include Paperclip::Glue
   include Mongoid::Document
   include Mongoid::Timestamps
-  #include Mongoid::Paperclip
 
   field :title, type: String
   field :body, type: String, default: ''
   field :hero_image_file_name, type: String
+  field :slug, type: String
+  index({ slug: 1 }, { unique: true, name: "slug_index" })
+
+  before_save :generate_slug
+
 
   has_attached_file :hero_image, styles: { main: "770x300!", thumbnail: "100x75" }
 
 
   def printable_post_date
     created_at.strftime("%m/%d/%y")
+  end
+
+private
+  def generate_slug
+    unless slug && !title
+      self.slug = title.downcase.gsub(/[^0-9a-z ]/i, '').gsub(' ','-')
+    end
   end
 end
