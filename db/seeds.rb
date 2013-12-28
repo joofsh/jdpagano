eef8d0b6d8eff12f1ac73fedb708140c91b71ce6
jd pagano <jonathanpagano@gmail.com>
2013-12-28 13:07:36 -0500

    Refactor seed data. Adding email to the initial user and making a better set of published and unpublished articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,20 @@
 
 # Create initial admin user
-u = User.new(name: 'JD Pagano', admin: true)
+u = User.new(name: 'JD Pagano', email: 'jonathanpagano@gmail.com', admin: true)
 u.password= 'orangeranch3'
 u.save!
 
 
-# Create dummy articles
+# Create 2 blogs
 tech = Blog.create(title: 'Tech', subdomain: 'tech')
 travel = Blog.create(title: 'Travel', subdomain: 'travel')
 
-5.times do |i|
+# Create dummy articles
+7.times do |i|
   Article.create(
     description: "desc of art ##{i}",
     keywords: %w(foo bar baz),
-    published: i.odd?,
+    published: i < 4,
     body: "This is the body of article ##{i}. This is a really fascinating article",
     title: "Title of art ##{i}",
     blog_id: (i.odd? ? travel.id : tech.id)
