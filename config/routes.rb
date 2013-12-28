d6703195d8cec245c5d63b28f283e8503edcaebb
jd pagano <jonathanpagano@gmail.com>
2013-12-28 12:15:38 -0500

    Add new constraints to strip out www from subdomain and to redirect to tech subdomain if an invalid one is presented

--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,39 @@
+# Dynamically build regex including a zero-width negative lookahead for each subdomain we use
+subdomains = Blog.pluck(:subdomain) # array of all subdomains used
+subdomain_str = '|^(?!'
+subdomains.each { |s| subdomain_str << "#{s}|" }
+subdomain_str = subdomain_str[0..-2]
+subdomain_str << ").*"
+
+BLANK_STRING_PATTERN = "^$"            # matches blank subdomain
+SUBDOMAIN_PATTERN = Regexp.new(BLANK_STRING_PATTERN + subdomain_str)
+
+
 Jdpagano::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-   root 'main#index'
-
-   get 'login' => 'sessions#new', as: 'login'
-   get 'logout' => 'sessions#destroy', as: 'logout'
-   resources :sessions
-   resources :articles, except: [:show]
-   get 'articles/:slug' => 'articles#show'
+  # Removes the www. from the beginning of the request URL,
+  # but maintains the rest of the subdomain if one exists
+  constraints subdomain: /^www/ do
+    get '(*any)' => redirect { |p, request| request.url.sub('www.','') }
+  end
 
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # Checks for a blank or non-Blog subdomain
+  # If there is no subdomain or an invalid one, we redirect to the tech blog
+  constraints subdomain: SUBDOMAIN_PATTERN do |p, req|
+    get '(*any)' => redirect(subdomain: 'tech', path: '/%{any}/'), any: /.*/
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  root 'main#index'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  get 'login' => 'sessions#new', as: 'login'
+  get 'logout' => 'sessions#destroy', as: 'logout'
+  resources :sessions
+  resources :articles, except: [:show]
+  get 'articles/:slug' => 'articles#show'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 
   get '*path', to: 'main#missing'
 end
